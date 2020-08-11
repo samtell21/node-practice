98dbd35c1f4981ab20455460b1fd9df97663e1cc
Sam <samtell21@gmail.com>
2020-08-11 11:04:30 -0400

    deleting dad

--- node-rest-shop/api/routes/dad.js
+++ node-rest-shop/api/routes/dad.js
@@ -5,4 +5,16 @@ router.get('/', (q,s,n) => {
     s.status(200).json("check it out, I made a website");
 });
 
+
+router.delete('/', (q,s,n) => {
+    s.status(200).json({
+        amessage: "you've been deleted",
+        array: [
+            "how do you feel?",
+            "a little cold?",
+            "a little afraid?"
+        ]
+    });
+});
+
 module.exports = router;
